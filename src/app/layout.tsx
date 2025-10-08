bf2e68ef09990818aa67dd9575ec9a96507a0aad
romatorres <romatorres12@gmail.com>
2025-10-08 18:10:37 -0300

    correção layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
-import type { Metadata } from "next";                                                                                                                                              │
-import { Paytone_One, Nunito } from "next/font/google";                                                                                                                            │
+import type { Metadata } from "next";
+import { Paytone_One, Nunito } from "next/font/google";
 import "./globals.css";
 import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 import { SessionProvider } from "@/components/site/_components/SessionProvider";
 import { Session } from "@/store/session-store"; // FIX 1: Added missing import
-import { Toaster } from "sonner"; 
+import { Toaster } from "sonner";
 
 const paytoneOne = Paytone_One({
   variable: "--font-paytone-one",
@@ -29,7 +29,6 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
   const readonlyHeaders = await headers();
   const mutableHeaders = new Headers(readonlyHeaders);
 
